781c0a1f1c49b37f25a7933096c324ed7777648f
Andrzej Borucki <borneq@example.com>
2013-09-02 11:15:06 +0200

    scan one dir; distribute files and dirs to different lists

---
+++ src/com/borneq/jcat/Main.java
@@ -0,0 +1,25 @@
+package com.borneq.jcat;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		scan(new File("c:\\temp"));
+	}
+
+	private static void scan(File aStartingDir)
+	{
+		File[] filesAndDirs = aStartingDir.listFiles();
+		String name = filesAndDirs[0].getName();
+		List<File> fileList = new ArrayList<File>();
+		List<File> dirList = new ArrayList<File>();
+		for(File file : filesAndDirs) {
+			if (file.isDirectory())
+				dirList.add(file);
+			else
+				fileList.add(file);		
+		}
+	}
+}
