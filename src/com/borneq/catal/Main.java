fd828ecaaa6e2e6d371c76353200da4509b8115f
Andrzej Borucki <borneq@example.com>
2013-09-02 15:21:13 +0200

    Console from [illegal argument exception]

--- src/com/borneq/catal/Main.java
+++ src/com/borneq/catal/Main.java
@@ -1,10 +1,15 @@
 package com.borneq.catal;
 
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import illegalargumentexception.console.TextDevice;
+import illegalargumentexception.console.TextDevices;
 
 public class Main {
-
+	private static final TextDevice io = TextDevices.defaultTextDevice();
+	
 	public static void main(String[] args) {
 		scan(new File("c:\\temp"));
 	}
@@ -12,7 +17,7 @@ public class Main {
 	private static void scan(File aStartingDir)
 	{
 		File[] filesAndDirs = aStartingDir.listFiles();
-		String name = filesAndDirs[0].getName();
+		//String name = filesAndDirs[0].getName();
 		List<File> fileList = new ArrayList<File>();
 		List<File> dirList = new ArrayList<File>();
 		for(File file : filesAndDirs) {
@@ -21,5 +26,7 @@ public class Main {
 			else
 				fileList.add(file);		
 		}
+		for(File file : fileList)
+			io.printf("%s\n",file.getPath());			 
 	}
 }
